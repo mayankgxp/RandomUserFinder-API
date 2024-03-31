935cce39a839ba4573ca894caf377b079a28d290
mayankgxp <mayankg303@Gmail.com>
2024-03-31 23:42:57 +0530

    Add files via upload

---
+++ RandomUserFinder-API/Models/RandomUser.cs
@@ -0,0 +1,38 @@
+﻿#nullable enable
+namespace RandomUserFinder.Models;
+public class Name
+{
+    public string Title { get; set; }
+    public string First { get; set; }
+    public string Last { get; set; }
+}
+
+public class Dob
+{
+    public DateTime Date { get; set; }
+    public int Age { get; set; }
+}
+
+public class Picture
+{
+    public string Large { get; set; }
+    public string Medium { get; set; }
+    public string Thumbnail { get; set; }
+}
+
+public class RandomeUserInfo
+{
+    public string Gender { get; set; }
+    public Name Name { get; set; }
+    public string Email { get; set; }
+    public Dob Dob { get; set; }
+    public string Phone { get; set; }
+    public object Id { get; set; }
+    public Picture Picture { get; set; }
+
+}
+
+public class RandomUser
+{
+    public List<RandomeUserInfo> Results { get; set; }
+}
