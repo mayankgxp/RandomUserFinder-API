e7663b16a17ce22c514c28e30cb17d0cb466ae0d
mayankgxp <mayankg303@Gmail.com>
2024-03-31 23:45:51 +0530

    Add files via upload

---
+++ RandoUserFinder.Tests/AuthControllerTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using RandomUserFinder.Controllers;
+using RandomUserFinder.Models;
+using RandomUserFinder.Services;
+using Xunit;
+
+namespace RandomUserFinder.Tests;
+public class AuthControllerTests
+{
+    [Fact]
+    public void Post_Returns_True_With_Valid_Credentials()
+    {
+        // Arrange
+        var authServiceMock = new Mock<IAuthService>();
+        authServiceMock.Setup(service => service.ValidateCredentials("valid_username", "valid_password")).Returns(true);
+
+        var controller = new AuthController(authServiceMock.Object);
+        var credentials = new Credential { UserName = "valid_username", Password = "valid_password" };
+
+        // Act
+        var result = controller.Post(credentials);
+
+        // Assert
+        var actionResult = Assert.IsType<ActionResult<bool>>(result);
+        var value = Assert.IsType<bool>(actionResult.Value);
+        Assert.True(value);
+    }
+
+    [Fact]
+    public void Post_Returns_False_With_Invalid_Credentials()
+    {
+        // Arrange
+        var authServiceMock = new Mock<IAuthService>();
+        authServiceMock.Setup(service => service.ValidateCredentials("invalid_username", "invalid_password")).Returns(false);
+
+        var controller = new AuthController(authServiceMock.Object);
+        var credentials = new Credential { UserName = "invalid_username", Password = "invalid_password" };
+
+        // Act
+        var result = controller.Post(credentials);
+
+        // Assert
+        var actionResult = Assert.IsType<ActionResult<bool>>(result);
+        var value = Assert.IsType<bool>(actionResult.Value);
+        Assert.False(value);
+    }
+}
---
+++ RandoUserFinder.Tests/UserControllerTests.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Extensions.Configuration;
+using Moq;
+using Newtonsoft.Json;
+using RandomUserFinder.Models;
+using RandomUserFinder.Services;
+using RandomUserFinder.Tests.Fixtures;
+using RandomUserFinder.Tests.Handlers;
+using Xunit;
+
+namespace RandomUserFinder.Tests;
+
+public class UserControllerTests : IClassFixture<UserControllerFixture>
+{
+
+    private const string RandomUserAPIUrl = "RandomUserAPIUrl";
+    public UserControllerTests()
+    {
+    }
+
+    [Fact]
+    public async Task GetRandomUser_Returns_Ok_With_Valid_RandomUser()
+    {
+        // Arrange
+        var expectedRandomUser = new RandomUser
+        {
+            Results = new List<RandomeUserInfo>() { new RandomeUserInfo() { Name = new Name() { First = "Mayank", Last = "Gupta" } } }
+        };
+        var expectedResponseJson = JsonConvert.SerializeObject(expectedRandomUser);
+
+        var httpClientFactoryMock = new Mock<IHttpClientFactory>();
+        httpClientFactoryMock.Setup(factory =>
+            factory.CreateClient(It.IsAny<string>())).Returns(new HttpClient(new MockHttpMessageHandler(expectedResponseJson)));
+
+        var configurationHook = new Mock<IConfiguration>();
+        configurationHook.Setup(x => x[RandomUserAPIUrl]).Returns("https://randomuser.me/api/");
+
+        var randomUserService = new RandomUserService(httpClientFactoryMock.Object, configurationHook.Object);
+
+        // Act
+        var result = await randomUserService.GetRandomUserAsync();
+
+        // Assert
+        Assert.NotNull(result);
+    }
+
+    [Fact]
+    public async Task GetRandomUser_Returns_NotFound_When_No_RandomUser()
+    {
+        // Arrange
+        RandomUser? expectedRandomUser = null;
+
+        var expectedResponseJson = JsonConvert.SerializeObject(expectedRandomUser);
+
+        var httpClientFactoryMock = new Mock<IHttpClientFactory>();
+        httpClientFactoryMock.Setup(factory =>
+            factory.CreateClient(It.IsAny<string>())).Returns(new HttpClient(new MockHttpMessageHandler(expectedResponseJson)));
+
+        var configurationHook = new Mock<IConfiguration>();
+        configurationHook.Setup(x => x[RandomUserAPIUrl]).Returns("https://randomuser.me/api/");
+
+
+        var randomUserService = new RandomUserService(httpClientFactoryMock.Object, configurationHook.Object);
+
+        // Act
+        var result = await randomUserService.GetRandomUserAsync();
+
+        // Assert
+        Assert.Null(result);
+    }
+}
